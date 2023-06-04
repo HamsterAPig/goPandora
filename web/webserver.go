81b92be7d2439b0b898d4da4c73f599e0ab3f530
HamsterAPig <102395304+HamsterAPig@users.noreply.github.com>
2023-06-04 08:32:27 +0800

    :adhesive_bandage:gin 模板的占位符重复，修改为{[]}的形式

--- web/webserver.go
+++ web/webserver.go
@@ -12,6 +12,7 @@ import (
 
 func ServerStart(address string, chatGPTAPI string) {
 	router := gin.Default()
+	router.Delims("{[", "]}")
 	// 注册自定义模板函数
 	router.SetFuncMap(template.FuncMap{
 		"safe": func(s string) template.HTML {
@@ -28,14 +29,20 @@ func ServerStart(address string, chatGPTAPI string) {
 		},
 	})
 
-	router.LoadHTMLFiles("web/gin/templates/404.html")
+	router.LoadHTMLFiles("web/gin/templates/login.html")
 
 	router.Static("/_next", "web/gin/static/_next")
 	router.Static("/fonts", "web/gin/static/fonts")
 	router.Static("/ulp", "web/gin/static/ulp")
 
-	router.GET("/404.html", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "404.html", gin.H{
+	//router.GET("/404.html", func(c *gin.Context) {
+	//	c.HTML(http.StatusOK, "404.html", gin.H{
+	//		"pandora_sentry": "false",
+	//		"api_prefix":     chatGPTAPI,
+	//	})
+	//})
+	router.GET("/auth/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", gin.H{
 			"pandora_sentry": "false",
 			"api_prefix":     chatGPTAPI,
 		})
