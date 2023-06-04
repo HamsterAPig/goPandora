3152b1fbf17b10c4eedc42effb4434d9d4487ed6
HamsterAPig <102395304+HamsterAPig@users.noreply.github.com>
2023-06-05 07:58:30 +0800

    :sparkles:添加accounts路由即路由处理

--- web/webserver.go
+++ web/webserver.go
@@ -48,6 +48,7 @@ func ServerStart(address string, param *PandoraParam) {
 
 	// 配置路由
 	router.GET("/api/auth/session", sessionAPIHandler)
+	router.GET("/api/accounts/check/v4-2023-04-27", checkAPIHandler)
 
 	router.GET("/", func(c *gin.Context) {
 		chatHandler(c, param, "")
@@ -69,6 +70,67 @@ func ServerStart(address string, param *PandoraParam) {
 	if err != nil {
 		return
 	}
+}
+
+func checkAPIHandler(c *gin.Context) {
+	// 下面这组数据是从Pandora中直接拿出来的
+	data := gin.H{
+		"accounts": gin.H{
+			"default": gin.H{
+				"account": gin.H{
+					"account_user_role": "account-owner",
+					"account_user_id":   "d0322341-7ace-4484-b3f7-89b03e82b927",
+					"processor": gin.H{
+						"a001": gin.H{
+							"has_customer_object": true,
+						},
+						"b001": gin.H{
+							"has_transaction_history": true,
+						},
+					},
+					"account_id": "a323bd05-db25-4e8f-9173-2f0c228cc8fa",
+					"is_most_recent_expired_subscription_gratis": true,
+					"has_previously_paid_subscription":           true,
+				},
+				"features": []string{
+					"model_switcher",
+					"model_preview",
+					"system_message",
+					"data_controls_enabled",
+					"data_export_enabled",
+					"show_existing_user_age_confirmation_modal",
+					"bucketed_history",
+					"priority_driven_models_list",
+					"message_style_202305",
+					"layout_may_2023",
+					"plugins_available",
+					"beta_features",
+					"infinite_scroll_history",
+					"browsing_available",
+					"browsing_inner_monologue",
+					"browsing_bing_branding",
+					"shareable_links",
+					"plugin_display_params",
+					"tools3_dev",
+					"tools2",
+					"debug",
+				},
+				"entitlement": gin.H{
+					"subscription_id":         "d0dcb1fc-56aa-4cd9-90ef-37f1e03576d3",
+					"has_active_subscription": true,
+					"subscription_plan":       "chatgptplusplan",
+					"expires_at":              "2089-08-08T23:59:59+00:00",
+				},
+				"last_active_subscription": gin.H{
+					"subscription_id":          "d0dcb1fc-56aa-4cd9-90ef-37f1e03576d3",
+					"purchase_origin_platform": "chatgpt_mobile_ios",
+					"will_renew":               true,
+				},
+			},
+		},
+		"temp_ap_available_at": "2023-05-20T17:30:00+00:00",
+	}
+	c.JSON(http.StatusOK, data)
 }
 
 // sessionAPIHandler 获取用户信息接口
