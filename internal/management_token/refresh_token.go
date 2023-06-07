5e31388db8e6f0cad196521d04d7899caaa6e959
HamsterAPig <102395304+HamsterAPig@users.noreply.github.com>
2023-06-07 09:07:34 +0800

    :tada:添加refresh token功能模块

---
+++ internal/management_token/refresh_token.go
@@ -0,0 +1,67 @@
+package management_token
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type RefreshData struct {
+	RedirectUri  string `json:"redirect_uri"`
+	GrantType    string `json:"grant_type"`
+	ClientId     string `json:"client_id"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+func refreshPostToken(url string, data RefreshData, userAgent string) (*http.Response, error) {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, fmt.Errorf("编码数据失败: %v", err)
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, fmt.Errorf("创建请求失败: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("User-Agent", userAgent)
+	client := &http.Client{}
+	rep, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("发送请求失败: %v", err)
+	}
+
+	return rep, nil
+}
+
+func GetTokenByRefreshToken(RefreshToken string, userAgent string) (string, error) {
+	data := RefreshData{
+		RedirectUri: "com.openai.chat://auth0.openai.com/ios/com.openai.chat/callback",
+		GrantType:   "refresh_token",
+		ClientId:    "pdlLIX2Y72MIl2rhLhTE9VV9bN905kBh",
+	}
+	data.RefreshToken = RefreshToken
+	url := "https://auth0.openai.com/oauth/token"
+	rep, err := refreshPostToken(url, data, userAgent)
+	if err != nil {
+		return "", err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+		}
+	}(rep.Body)
+	if 200 != rep.StatusCode {
+		return "", fmt.Errorf("获取Token失败: %v", rep.StatusCode)
+	}
+	body, err := io.ReadAll(rep.Body)
+	var jsonData map[string]interface{}
+	err = json.Unmarshal([]byte(body), &jsonData)
+	if err != nil {
+		return "", fmt.Errorf("解析JSON失败: %v", err)
+	}
+	accessToken := jsonData["access_token"].(string)
+	return accessToken, nil
+}
