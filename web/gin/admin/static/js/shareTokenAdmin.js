a117bd8b88308aef22cd3100071eaf453f20514b
HamsterAPig <102395304+HamsterAPig@users.noreply.github.com>
2023-06-15 09:28:46 +0800

    :seedling:直接从服务器的接口请求数据

--- web/gin/admin/static/js/shareTokenAdmin.js
+++ web/gin/admin/static/js/shareTokenAdmin.js
@@ -1,21 +1,22 @@
 function renderShareTokenTable(data) {
-    const tableBody = document.querySelector('#shareTokenTable tbody');
-
-    data.forEach(token => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-        <th scope="row"><input class="form-check-input me-1" type="checkbox" value=""></th>
-        <td>${token.ID}</td>
-        <td>${token.UserID}</td>
-        <td>${token.UniqueName}</td>
-        <td>${token.ExpiresAt}</td>
-        <td>${token.SiteLimit}</td>
-        <td>${token.SK}</td>
-        <td>${token.UpdateTime}</td>
-        <td>${token.Comment}</td>
-    `;
-        tableBody.appendChild(row);
-    });
+    getDataFromAPI().then(data => {
+        let tableBody = document.querySelector('#shareTokenTable tbody');
+        data.forEach(token => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+            <th scope="row"><input class="form-check-input me-1" type="checkbox" value=""></th>
+            <td>${token.ID}</td>
+            <td>${token.UserID}</td>
+            <td>${token.UniqueName}</td>
+            <td>${token.ExpiresAt}</td>
+            <td>${token.SiteLimit}</td>
+            <td>${token.SK}</td>
+            <td>${token.UpdateTime}</td>
+            <td>${token.Comment}</td>
+        `;
+            tableBody.appendChild(row);
+        });
+    })
 }
 
 function bindSelectAllCheckbox() {
@@ -62,6 +63,22 @@ function handleSubmitForm() {
     });
 }
 
-renderShareTokenTable(shareTokenData);// 显示所有来自于json的数据
+async function getDataFromAPI(url) {
+    // todo))这里是构建一组虚拟的数据，发布之后把它删掉
+    return shareTokenData;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error:', error.message);
+        return null;
+    }
+}
+
+renderShareTokenTable();// 显示所有来自于json的数据
 bindSelectAllCheckbox();// 监听复选框
 handleSubmitForm(); // 提交表单
